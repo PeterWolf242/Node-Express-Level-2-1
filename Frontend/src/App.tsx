f1c2669341f36bf656544572f3842e4156eaebdd
[PeterWolf242] <[peter.ebm@web.de]>
2025-04-01 16:00:52 +0200

    Node-Express-Level-2-1

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+interface IPerson {
+	id: number;
+	name: string;
+}
+
+interface IStarship {
+	id: number;
+	name: string;
+}
+
+
+function App() {
+
+	const [person, SetPerson] = useState<IPerson[]>([]);
+	const [ship, SetShip] = useState<IStarship[]>([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const resp = await axios.get("http://localhost:3000/people")
+			console.log(resp.data);
+			SetPerson(resp.data);
+		}
+		fetchData();
+	}, [])
+
+	useEffect(() => {
+		async function fetchData() {
+			const resp = await axios.get("http://localhost:3000/starships")
+			console.log(resp.data);
+			SetShip(resp.data);
+		}
+		fetchData();
+	}, [])
+
+
+
+	return (
+		<>
+			{person.map((person) => (
+				<p>{person.name}</p>
+			))}
+			{ship.map((ship) => (
+				<h2>{ship.name}</h2>
+			))}
+		</>
+	)
+}
+
+export default App
